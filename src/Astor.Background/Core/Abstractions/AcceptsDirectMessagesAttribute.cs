216e99b01732b5856f2e22cbc86db37a2eed95a7
tarasov_es <pavluchenk0@yandex.ru>
2021-04-06 20:44:07 +0300

    maybe this name

---
+++ src/Astor.Background/Core/Abstractions/AcceptsDirectMessagesAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Astor.Background.Core.Abstractions
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AcceptsDirectMessagesAttribute : Attribute
+    {
+    }
+}
