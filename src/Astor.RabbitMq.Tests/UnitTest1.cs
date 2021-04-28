860820f5301912df8b9ca72e638934e4c74457d8
astor <pavluchenk0@yandex.ru>
2021-04-28 19:41:32 +0300

    started from rabbitmq

---
+++ src/Astor.RabbitMq.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Astor.RabbitMq.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
