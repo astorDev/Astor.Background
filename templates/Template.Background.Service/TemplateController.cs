f6770ffd4f843fda710cef256c6adaf893b84218
tarasov_es <pavluchenk0@yandex.ru>
2021-05-11 17:17:48 +0300

    background service template added

---
+++ templates/Template.Background.Service/TemplateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Astor.Background.Core.Abstractions;
+
+namespace Template.Background.Service
+{
+    public class TemplateController
+    {
+        [Astor.Background.RabbitMq.Abstractions.SubscribedOn("numbers.supply", DeclareExchange = true)]
+        public Task<int> TryParse(string num)
+        {
+            Int32.TryParse(num, out var n);
+            return Task.FromResult(n);
+        }
+        
+        [RunsEvery("00:00:10")]
+        public Task<string> GetTime()
+        {
+            return Task.FromResult($"Current Time is {DateTime.Now}");
+        }
+    }
+}
